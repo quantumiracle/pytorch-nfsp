55f1f3e6d32a145c31da72160bfdcc0f8003683f
zihan <zhding@mail.ustc.edu.cn>
2021-05-06 00:42:44 -0400

    exp

--- train.sh
+++ train.sh
@@ -10,5 +10,5 @@ echo "Save as: " $DATE
 #python train_dqn_against_baseline.py --env SlimeVolley-v0 --hidden-dim 256 --train-freq 100 --batch-size 256 --max-frames 1000000000 > log/$DATE$RAND.log &
 #python train_dqn_against_baseline.py --env SlimeVolleyNoFrameskip-v0 --hidden-dim 512 --max-frames 30000000 > log/$DATE$RAND.log &
 # python train_dqn_against_baseline_mp.py --env SlimeVolley-v0 --num-envs 5 --hidden-dim 256 --train-freq 100 --batch-size 1024 --max-frames 500000000 > log/$DATE$RAND.log &
-python train_dqn_against_baseline_mp.py --env SlimeVolley-v0 --gamma 0.999 --eps-decay 1000000 --buffer-size 1000000 --num-envs 10 --hidden-dim 256 --train-freq 100 --batch-size 1024 --max-frames 500000000 > log/$DATE$RAND.log &
+python train_dqn_against_baseline_mp.py --env SlimeVolley-v0 --gamma 0.99 --eps-decay 1000000 --buffer-size 1000000 --num-envs 10 --hidden-dim 256 --train-freq 10 --batch-size 1024 --max-frames 500000000 > log/$DATE$RAND.log &
 # python nash_dqn.py --env SlimeVolley-v0 --num-envs 2  --hidden-dim 256 --train-freq 100 --batch-size 1024 --max-frames 500000000 > log/$DATE$RAND.log &
