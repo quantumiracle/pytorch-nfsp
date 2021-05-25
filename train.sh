162fa167aaca29d6a34e97dfc5dbdf430da5f6a2
quantumiracle <1402434478@qq.com>
2021-05-25 19:51:11 +0800

    test nash_dqn for pong

--- train.sh
+++ train.sh
@@ -16,4 +16,5 @@ echo "Save as: " $DATE
 # python nash_dqn.py --env SlimeVolley-v0 --num-envs 3  --hidden-dim 256 --evaluation-interval 50000 --train-freq 100 --batch-size 1024 --max-frames 500000000 --eps-final 0.01 --gamma 0.99 --eps-decay 1000000 > log/$DATE$RAND.log &
 # python train_dqn_against_baseline.py  --env Pong-ram-v0 --hidden-dim 64  --max-tag-interval 10000   > log/$DATE$RAND.log &
 # python nash_dqn.py --env rps_v1 --num-envs 2 --hidden-dim 64 --evaluation-interval 500 --rl-start 1000 > log/$DATE$RAND.log &
-python bilateral_dqn.py --env pong_v1 --num-envs 3 --ram --evaluation-interval 20000 --max-tag-interval 10000 --max-frames 20000000  --hidden-dim 256 > log/$DATE$RAND.log &
+# python bilateral_dqn.py --env pong_v1 --num-envs 3 --ram --evaluation-interval 20000 --max-tag-interval 10000 --max-frames 20000000  --hidden-dim 256 > log/$DATE$RAND.log &
+python nash_dqn.py --env pong_v1 --num-envs 3 --ram --hidden-dim 256 --evaluation-interval 50000 --max-tag-interval 10000 --train-freq 100 --batch-size 1024 --max-frames 500000000 > log/$DATE$RAND.log &
